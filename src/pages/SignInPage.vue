2438c1f5c20e5c96758618932e1c38b533c13a99
Shist <shist.pupust@mail.ru>
2024-04-23 14:41:09 +0300

    Added layout of Sign-In page

--- src/pages/SignInPage.vue
+++ src/pages/SignInPage.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="sign-in-page">
-    <h1 class="sign-in-page__headline">Sign In</h1>
+    <form action="#" class="sign-in-page__form">
+      <h2 class="sign-in-page__headline">Sign in</h2>
+      <label class="sign-in-page__email-input-label" for="emailInput">
+        Enter your email below:
+      </label>
+      <input
+        type="email"
+        name="email"
+        class="sign-in-page__email-input"
+        id="emailInput"
+        placeholder="Email"
+        required
+      />
+      <label class="sign-in-page__password-input-label" for="passwordInput">
+        Enter your password below:
+      </label>
+      <input
+        type="password"
+        name="password"
+        class="sign-in-page__password-input"
+        id="passwordInput"
+        placeholder="Password"
+        required
+      />
+      <button class="sign-in-page__confirm-btn" type="submit">Confirm</button>
+    </form>
+    <h3 class="sign-in-page__sign-up-suggestion-headline">
+      Don't have account yet?
+    </h3>
+    <a class="sign-in-page__sign-up-btn" @click="this.$router.push('/sign-up')">
+      Sign up
+    </a>
   </div>
 </template>
 
@@ -14,10 +45,53 @@ export default {
 @import "@/styles/global";
 
 .sign-in-page {
+  flex-grow: 1;
+  @extend %default-wrapper;
   display: flex;
   flex-direction: column;
-  &__headline {
-    @include default-headline(36px, 36px, $color-black);
+  align-items: center;
+  &__form {
+    display: flex;
+    flex-direction: column;
+    .sign-in-page__headline {
+      @include default-headline(42px, 42px, $color-black);
+      margin-bottom: 30px;
+      text-align: center;
+    }
+    .sign-in-page__email-input-label {
+      @include default-text(24px, 24px, $color-black);
+      margin-bottom: 10px;
+      @media (max-width: $phone-l) {
+        font-size: 20px;
+        line-height: 20px;
+      }
+    }
+    .sign-in-page__email-input {
+      @extend %default-input;
+      margin-bottom: 20px;
+    }
+    .sign-in-page__password-input-label {
+      @include default-text(24px, 24px, $color-black);
+      margin-bottom: 10px;
+      @media (max-width: $phone-l) {
+        font-size: 20px;
+        line-height: 20px;
+      }
+    }
+    .sign-in-page__password-input {
+      @extend %default-input;
+      margin-bottom: 40px;
+    }
+    .sign-in-page__confirm-btn {
+      @include default-btn(300px, $color-white, $color-orange);
+      margin-bottom: 20px;
+    }
+  }
+  &__sign-up-suggestion-headline {
+    @include default-text(20px, 20px, $color-black);
+  }
+  &__sign-up-btn {
+    @include default-btn(100px, $color-white, $color-orange);
   }
 }
 </style>
