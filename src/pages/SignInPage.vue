36531d2ca7f3164be96d68a36014b0e2c4a2f077
Shist <shist.pupust@mail.ru>
2024-04-24 16:27:21 +0300

    Added authorization for users (by email and password)

--- src/pages/SignInPage.vue
+++ src/pages/SignInPage.vue
@@ -26,7 +26,13 @@
         placeholder="Password"
         required
       />
-      <button class="sign-in-page__confirm-btn" type="submit">Confirm</button>
+      <button
+        class="sign-in-page__confirm-btn"
+        type="submit"
+        @click.prevent="onConfirmBtnClicked"
+      >
+        Confirm
+      </button>
     </form>
     <h3 class="sign-in-page__sign-up-suggestion-headline">
       Don't have an account yet?
@@ -38,9 +44,76 @@
 </template>
 
 <script>
+import { toast } from "vue3-toastify";
+import { mapState, mapMutations, mapActions } from "vuex";
+
 export default {
   name: "sign-in-page",
+  methods: {
+    ...mapMutations({
+      setCurrToastId: "toast/setCurrToastId",
+    }),
+    ...mapActions({
+      fetchUserByEmail: "firebase/fetchUserByEmail",
+    }),
+    removeCurrToast() {
+      if (this.currToastId) {
+        toast.remove(this.currToastId);
+        this.setCurrToastId(null);
+      }
+    },
+    async onConfirmBtnClicked() {
+      this.removeCurrToast();
+
+      const loadingToastId = toast("Logging...", {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: false,
+        closeOnClick: false,
+        closeButton: false,
+      });
+      this.setCurrToastId(loadingToastId);
+
+      try {
+        await this.fetchUserByEmail(this.email);
+
+        if (!this.currUser) {
+          throw new Error(
+            "There is no user registered with this email! Specify another one or create a new account."
+          );
+        }
+
+        if (this.password !== this.currUser.password) {
+          throw new Error("The password you entered is incorrect!");
+        }
+
+        this.removeCurrToast();
+        const successToastId = toast("You have successfully logged in!", {
+          type: "success",
+          position: toast.POSITION.BOTTOM_CENTER,
+          closeOnClick: false,
+        });
+        this.setCurrToastId(successToastId);
+
+        this.$router.push("/");
+      } catch (error) {
+        this.removeCurrToast();
+        const errorToastId = toast(
+          `An error occurred while trying to log in to account! ${error.message}`,
+          {
+            type: "error",
+            position: toast.POSITION.BOTTOM_CENTER,
+            closeOnClick: false,
+          }
+        );
+        this.setCurrToastId(errorToastId);
+      }
+    },
+  },
   computed: {
+    ...mapState({
+      currToastId: (state) => state.toast.currToastId,
+      currUser: (state) => state.firebase.currUser,
+    }),
     email: {
       get() {
         return this.$store.state.signIn.email;
