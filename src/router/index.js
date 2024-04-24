55cae9de7fb14d5180c9565b21b7f90e5df88c8d
Shist <shist.pupust@mail.ru>
2024-04-24 17:05:50 +0300

    Added navigation guard which moves user to SignInPage if not authorized

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import MainPage from "../pages/MainPage.vue";
 import SignInPage from "../pages/SignInPage.vue";
 import SignUpPage from "../pages/SignUpPage.vue";
 import TaskByIdPage from "../pages/TaskByIdPage.vue";
+import store from "@/store";
 
 const routes = [
   {
@@ -26,6 +27,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to) => {
+  if (to.path !== "/sign-in" && to.path !== "/sign-up") {
+    if (!store.state.firebase.currUser) {
+      return "/sign-in";
+    }
+  }
+  return true;
 });
 
 export default router;
